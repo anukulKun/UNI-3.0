30296af6eec1dc0c2902b8ae62382429b7a01e37
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-08-24 19:30:51 +0530

    test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,11 +2,11 @@
 "use client"
 import React, { useState, useEffect } from 'react'
 import { useActiveAccount } from 'thirdweb/react'
-import Wallet from '@/components/thirdweb/Wallet'
+// import Wallet from '@/components/thirdweb/Wallet'
 
 // import Purpose from '@/components/thirdweb/propose'
 import GetPurpose from "@/components/thirdweb/getProposal"
-import { PurposeDao } from '@/components/component/purpose-dao'
+// import { PurposeDao } from '@/components/component/purpose-dao'
 
 export default function Page() {
   const [wallets, setWallets] = useState(null);
@@ -28,8 +28,7 @@ export default function Page() {
   return (
     <div>
       <GetPurpose wallets={wallets} />
-      <Wallet/>
-      <PurposeDao/>
+      
     </div>
   );
 }
